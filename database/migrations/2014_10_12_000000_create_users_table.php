12dabb5803a971b971fde5b1eaf2b054058f4174
deatrisya <mirelldee47@gmail.com>
2022-06-04 16:03:39 +0700

    create migration users and password reset

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('foto')->nullable();
+            $table->string('nama');
+            $table->string('username',20)->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->date('tgl_lahir');
+            $table->string('tempat_lahir');
+            $table->enum('jenis_kelamin',['L','P']);
+            $table->string('alamat');
             $table->rememberToken();
             $table->timestamps();
         });
