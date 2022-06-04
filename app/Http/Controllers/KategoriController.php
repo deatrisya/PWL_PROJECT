f5a4f06bc85297192784ca3b0eb569faacef0735
sitiaisyah14 <sitiaisyah4110@gmail.com>
2022-06-04 19:17:11 +0700

    model and controller table Kategori

---
+++ app/Http/Controllers/KategoriController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Kategori;
+use Illuminate\Http\Request;
+use RealRashid\SweetAlert\Facades\Alert;
+
+class KategoriController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $pagination = 5;
+        $kategori = Kategori::when($request->keyword, function($query) use ($request){
+            $query
+            ->where('nama_kategori','like',"%{$request->keyword}%");
+        })->orderBy('id')->paginate($pagination);
+
+
+        $kategori->appends($request->only('keyword'));
+        return view('kategori.kategoriIndex',compact('kategori'))
+            ->with('i',(request()->input('page',1)-1)*$pagination);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('kategori.kategoriCreate');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request -> validate([
+            'nama_kategori' => 'required|string'
+        ]);
+
+        Kategori::create($request->all());
+
+        Alert::success('Success','Data Kategori Berhasil Ditambahkan');
+        return redirect()->route('kategori.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $kategori = Kategori::find($id);
+        return view('kategori.kategoriDetail', compact('kategori'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $kategori = Kategori::find($id);
+        return view('kategori.kategoriEdit', compact('kategori'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request -> validate([
+            'nama_kategori' => 'required|string'
+        ]);
+
+        Kategori::find($id)->update($request->all());
+        Alert::success('Success','Data Kategori Berhasil Diupdate');
+        return redirect()->route('kategori.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Kategori::find($id)->delete();
+        Alert::success('Success','Data Kategori Berhasil DiHapus');
+        return redirect()->route('kategori.index');
+    }
+}
