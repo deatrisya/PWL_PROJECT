cea8c9613541f6cbd0f5dc8d40b338b9707186ad
sitiaisyah14 <sitiaisyah4110@gmail.com>
2022-06-05 14:41:04 +0700

    create migration table pemeliharaan

---
+++ database/migrations/2022_06_05_073642_pemeliharaan.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Pemeliharaan extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pemeliharaan', function (Blueprint $table) {
+            $table->id();
+            $table->date('tgl_pemeliharaan');
+            $table->double('jumlah');
+            $table->enum('status', ['Sedang Perbaikan','Selesai Perbaikan']);
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('barang_id')->nullable();
+            $table->foreign('barang_id')->references('id')->on('barang');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pemeliharaan');
+    }
+}
