b699eacaa523290155d7cb7af4918c5a354708d0
deatrisya <mirelldee47@gmail.com>
2022-06-11 22:18:23 +0700

    add pagination ruangan

--- resources/views/ruangan/ruanganIndex.blade.php
+++ resources/views/ruangan/ruanganIndex.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.app')
 @section('title')
-    Data Ruangan
+Data Ruangan
 @endsection
 @section('content')
 <div class="col-lg-12 grid-martin stretch-card">
@@ -8,25 +8,22 @@
         <div class="card-body">
             <h4 class="card-title">Data Ruangan</h4>
             <div class="button">
-               <div class="row">
-                   <div class="col-md-6">
-                    <div class="float-left">
-                        <a class="btn btn-success" href="{{route('ruangan.create')}}">+ Tambah Data</a>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="float-left">
+                            <a class="btn btn-success" href="{{route('ruangan.create')}}">+ Tambah Data</a>
+                        </div>
                     </div>
-                   </div>
-                   <div class="col-md-6 " >
-
-                    <div class="float-right">
-                        <form class="form-inline my-2 my-lg-0" action="{{url()->current()}}" method="GET">
-                            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="keyword" value="{{request('keyword')}}">
-                            <button class="btn btn-icons btn-warning" type="submit"><i class="mdi mdi-magnify"></i></button>
-                        </form>
-                   </div>
-
-                   <div class="d-flex flex-row-reverse float-right px-2">
-                    {{-- <a href="{{route('keuangan_pdf')}}" target="_blank" class="btn btn-icons btn-danger"> <i class="mdi mdi-file-document"></i> </a> --}}
-                   </div>
-               </div>
+                    <div class="col-md-6 ">
+                        <div class="float-right">
+                            <form class="form-inline my-2 my-lg-0" action="{{url()->current()}}" method="GET">
+                                <input class="form-control mr-sm-2" type="search" placeholder="Search"
+                                    aria-label="Search" name="keyword" value="{{request('keyword')}}">
+                                <button class="btn btn-icons btn-primary" type="submit"><i
+                                        class="mdi mdi-magnify"></i></button>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>
 
@@ -36,20 +33,24 @@
                         <tr>
                             <th scope="col">ID</th>
                             <th scope="col">Nama Ruangan</th>
+                            <th scope="col">Aksi</th>
                         </tr>
                     </thead>
                     <tbody>
                         @foreach ($ruangan as $data)
-                    <tr>
+                        <tr>
                             <td scope="row">{{ ++$i}}</td>
                             <td>{{$data->nama_ruangan}}</td>
                             <td>
                                 <form action="{{ route('ruangan.destroy',  $data->id) }}" method="POST">
-                                    <a class="btn btn-icons btn-primary" href="{{route('ruangan.show', $data->id)}}"><i class="mdi mdi-eye"></i></a>
-                                    <a class="btn btn-icons btn-warning" href="{{route('ruangan.edit', $data->id)}}"><i class="mdi mdi-pencil"></i></a>
+                                    <a class="btn btn-icons btn-primary" href="{{route('ruangan.show', $data->id)}}"><i
+                                            class="mdi mdi-eye"></i></a>
+                                    <a class="btn btn-icons btn-warning" href="{{route('ruangan.edit', $data->id)}}"><i
+                                            class="mdi mdi-pencil"></i></a>
                                     @csrf
                                     @method('DELETE')
-                                    <button type="submit" class="btn btn-icons btn-danger"><i class="mdi mdi-delete"></i></button>
+                                    <button type="submit" class="btn btn-icons btn-danger"><i
+                                            class="mdi mdi-delete"></i></button>
                                 </form>
                             </td>
                         </tr>
@@ -67,9 +68,9 @@
                                 Data Per Halaman : {!! $ruangan->perPage() !!} <br />
                             </p>
                         </div>
-                        <div class="mx-auto">
-                            <div class="paginate-button col-md-12">
-                                {{-- {!! $ruangan->links !!} --}}
+                        <div class="col-md-12">
+                            <div class="paginate-button float-right">
+                                {!! $ruangan->links() !!}
                             </div>
                         </div>
                     </div>
