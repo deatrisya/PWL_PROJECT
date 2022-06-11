37ee0e91949145875686dff3638cd398bce9ecc3
deatrisya <mirelldee47@gmail.com>
2022-06-11 22:21:51 +0700

    add pagination for supplier

--- resources/views/supplier/supplierIndex.blade.php
+++ resources/views/supplier/supplierIndex.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.app')
 @section('title')
-    Data Supplier
+Data Supplier
 @endsection
 @section('content')
 <div class="col-lg-12 grid-martin stretch-card">
@@ -8,25 +8,23 @@
         <div class="card-body">
             <h4 class="card-title">Data Supplier</h4>
             <div class="button">
-               <div class="row">
-                   <div class="col-md-6">
-                    <div class="float-left">
-                        <a class="btn btn-success" href="{{route('supplier.create')}}">+ Tambah Data</a>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="float-left">
+                            <a class="btn btn-success" href="{{route('supplier.create')}}">+ Tambah Data</a>
+                        </div>
                     </div>
-                   </div>
-                   <div class="col-md-6 " >
+                    <div class="col-md-6 ">
 
-                    <div class="float-right">
-                        <form class="form-inline my-2 my-lg-0" action="{{url()->current()}}" method="GET">
-                            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="keyword" value="{{request('keyword')}}">
-                            <button class="btn btn-icons btn-warning" type="submit"><i class="mdi mdi-magnify"></i></button>
-                        </form>
-                   </div>
-
-                   <div class="d-flex flex-row-reverse float-right px-2">
-                    {{-- <a href="{{route('keuangan_pdf')}}" target="_blank" class="btn btn-icons btn-danger"> <i class="mdi mdi-file-document"></i> </a> --}}
-                   </div>
-               </div>
+                        <div class="float-right">
+                            <form class="form-inline my-2 my-lg-0" action="{{url()->current()}}" method="GET">
+                                <input class="form-control mr-sm-2" type="search" placeholder="Search"
+                                    aria-label="Search" name="keyword" value="{{request('keyword')}}">
+                                <button class="btn btn-icons btn-primary" type="submit"><i
+                                        class="mdi mdi-magnify"></i></button>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>
 
@@ -38,22 +36,26 @@
                             <th scope="col">Logo Perusahaan</th>
                             <th scope="col">Nama Perusahaan</th>
                             <th scope="col">Alamat</th>
+                            <th scope="col">Aksi</th>
                         </tr>
                     </thead>
                     <tbody>
                         @foreach ($supplier as $data)
-                    <tr>
+                        <tr>
                             <td scope="row">{{ ++$i}}</td>
-                            <td><img width="100px" height="100px" src="{{ asset('storage/' . $data->logo) }}"></td>
+                            <td><img width="100px" height="100px" src="{{ asset('storage/' . $data->logo) }}" class="rounded-0 img-table"></td>
                             <td>{{$data->nama_perusahaan}}</td>
                             <td>{{$data->alamat}}</td>
                             <td>
                                 <form action="{{ route('supplier.destroy',  $data->id) }}" method="POST">
-                                    <a class="btn btn-icons btn-primary" href="{{route('supplier.show', $data->id)}}"><i class="mdi mdi-eye"></i></a>
-                                    <a class="btn btn-icons btn-warning" href="{{route('supplier.edit', $data->id)}}"><i class="mdi mdi-pencil"></i></a>
+                                    <a class="btn btn-icons btn-primary" href="{{route('supplier.show', $data->id)}}"><i
+                                            class="mdi mdi-eye"></i></a>
+                                    <a class="btn btn-icons btn-warning" href="{{route('supplier.edit', $data->id)}}"><i
+                                            class="mdi mdi-pencil"></i></a>
                                     @csrf
                                     @method('DELETE')
-                                    <button type="submit" class="btn btn-icons btn-danger"><i class="mdi mdi-delete"></i></button>
+                                    <button type="submit" class="btn btn-icons btn-danger"><i
+                                            class="mdi mdi-delete"></i></button>
                                 </form>
                             </td>
                         </tr>
@@ -71,9 +73,9 @@
                                 Data Per Halaman : {!! $supplier->perPage() !!} <br />
                             </p>
                         </div>
-                        <div class="mx-auto">
-                            <div class="paginate-button col-md-12">
-                                {{-- {!! $supplier->links !!} --}}
+                        <div class="col-md-12">
+                            <div class="paginate-button float-right">
+                                {!! $supplier->links() !!}
                             </div>
                         </div>
                     </div>
