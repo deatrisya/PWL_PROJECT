5a1fe39909c96341e5655c7d7917993eb3b31861
sitiaisyah14 <sitiaisyah4110@gmail.com>
2022-06-12 21:36:29 +0700

    Modify databaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
             KategoriSeeder::class,
             SupplierSeeder::class,
             BarangSeeder::class,
-            PengadaanSeeder::class
+            // PengadaanSeeder::class
         ]);
 
     }
