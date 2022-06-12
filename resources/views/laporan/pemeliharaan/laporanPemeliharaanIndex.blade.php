53b3b87484ef7ecd0f98412284bef9eece9619eb
sitiaisyah14 <sitiaisyah4110@gmail.com>
2022-06-12 21:40:24 +0700

    view Laporan Pemelihraan Index

---
+++ resources/views/laporan/pemeliharaan/laporanPemeliharaanIndex.blade.php
@@ -0,0 +1,57 @@
+@extends('layout.app')
+@section('title')
+Laporan Data Pemeliharaan Barang
+@endsection
+@section('content')
+<div class="col-lg-12 grid-martin stretch-card">
+    <div class="card">
+        <div class="card-body">
+            <h4 class="card-title">Laporan Data Pemeliharaan Barang</h4>
+            <div class="button">
+                <div class="row">
+                <div class="col-md-12">
+                    <div class="d-flex float-right">
+                        <div class="d-flex flex-row-reverse float-right px-2">
+                            <a href=" {{route('laporanPemeliharaan.cetakPdf')}} " target="_blank" class="btn btn-icons btn-danger"> <i class="mdi mdi-file-document"></i> </a>
+                        </div>
+                        <div class="ml-3">
+                                <form class="form-inline my-2 my-lg-0" action="{{url()->current()}}" method="GET">
+                                    <input class="form-control mr-sm-2" type="search" placeholder="Search"
+                                        aria-label="Search" name="keyword" value="{{request('keyword')}}">
+                                    <button class="btn btn-icons btn-primary" type="submit"><i
+                                            class="mdi mdi-magnify"></i></button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="table-responsive">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">No</th>
+                            <th scope="col">Tanggal Pemeliharaan</th>
+                            <th scope="col">Penanggung Jawab</th>
+                            <th scope="col">Barang</th>
+                            <th scope="col">Jumlah</th>
+                            <th scope="col">Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($pemeliharaan as $data)
+                        <tr>
+                            <td scope="row">{{++$i}}</td>
+                            <td>{{date('d-m-Y',strtotime($data->tgl_pemeliharaan))}}</td>
+                            <td>{{$data->user->nama}}</td>
+                            <td>{{$data->barang->nama_barang}}</td>
+                            <td>{{$data->jumlah}}</td>
+                            <td>{!!$data->statusPemeliharaan!!}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+    </div>
+</div>
+@endsection
